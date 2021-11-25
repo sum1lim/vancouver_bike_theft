fbb8a5c0299678ae53b6db2bb69efbc269a9e024
Sangwon Lim <angwonlim@gmail.com>
2021-11-24 21:41:54 -0800

    gitignore

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ __version__ = (0, 0, 0)
 
 setup(
     name="vancouver_bike_theft",
-    description="Machine/Deep Learning Architectures for Vancouver bike theft risk prediction/assessment model",
+    description="Data Mining for Vancouver bike theft risk prediction/assessment model",
     version=".".join(str(d) for d in __version__),
     author="Sangwon Lim",
     author_email="sangwonl@uvic.ca",
